4ae65c28861f3f42c9c6698dd85c2b933b213e63
SergiySvirkov <124861145+SergiySvirkov@users.noreply.github.com>
2023-11-28 14:33:12 +0200

    Create database.php

---
+++ config/database.php
@@ -0,0 +1,6 @@
+<?php
+// Database configuration
+define('DB_HOST', 'your_db_host');
+define('DB_NAME', 'your_db_name');
+define('DB_USER', 'your_db_user');
+define('DB_PASSWORD', 'your_db_password');
